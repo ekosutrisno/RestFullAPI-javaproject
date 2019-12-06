af1e215b398b9704dfc525ae9dc29cff94c00b55
ekosutrisno <51039205+ekosutrisno@users.noreply.github.com>
2019-12-06 21:15:22 +0700

    Loading data and file assets

---
+++ src/main/java/mvcapp/AppV2/AppV2Application.java
@@ -0,0 +1,13 @@
+package mvcapp.AppV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/mvcapp/AppV2/AppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mvcapp.AppV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
